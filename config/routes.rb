2d9073565595e29cd6520d959d0fee893ad10ec0
Nozomi Ishii <non.ankt@gmail.com>
2019-09-24 02:22:43 +0900

    added cv page'

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'works/index' => 'works#index'
   get 'works/:id' => 'works#show'
   get '/' => 'home#top'
+  get 'cv' => 'home#cv'
   get 'about' => 'home#about'
 end
