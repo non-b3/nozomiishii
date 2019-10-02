513005376ec42c2cdb32aa7fff5c4590351616d2
Nozomi Ishii <non.ankt@gmail.com>
2019-10-03 04:26:49 +0900

    erase cv from route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'works/:id' => 'works#show'
   get 'test' => 'home#test'
   get '/' => 'home#top'
-  get 'cv' => 'home#cv'
+  # get 'cv' => 'home#cv'
   get 'about' => 'home#about'
 end
