44ba8d6c02983deb65ab7289bd096eab6056a630
Nozomi Ishii <non.ankt@gmail.com>
2019-10-18 02:19:22 +0900

    change the news bar color

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'codes/:id' => 'codes#show'
   get 'codes/index' => 'codes#index'
+  get 'codes/:id' => 'codes#show'
 
-  get 'works/index' => 'works#index'
-  get 'works/:id' => 'works#show'
-  get 'test' => 'home#test'
   get '/' => 'home#top'
 
   get 'about' => 'home#about'
