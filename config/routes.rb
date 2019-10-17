ff634258ad6f83f7d926b88caf4c5b8bfdfd701f
Nozomi Ishii <non.ankt@gmail.com>
2019-10-17 20:58:55 +0900

    try to make nozomiishii.jp/code/:id

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'codes/:id' => 'codes#show'
+  get 'codes/index' => 'codes#index'
+
   get 'works/index' => 'works#index'
   get 'works/:id' => 'works#show'
   get 'test' => 'home#test'
   get '/' => 'home#top'
-  # get 'cv' => 'home#cv'
+
   get 'about' => 'home#about'
 end
