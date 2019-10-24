c2ea09f98d529414d720069261c431c646d297eb
Nozomi Ishii <non.ankt@gmail.com>
2019-10-24 21:22:38 +0900

    deploy day4

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,17 @@ Code.delete_all
 puts 'Deleted all the code data'
 
 [
+  # ['14', '3rd NOV', ''],
+  # ['13', '2nd NOV', ''],
+  # ['12', '1st NOV', ''],
+  # ['11', '31th OCT', ''],
+  # ['10', '30th OCT', ''],
+  # ['9', '29th OCT', ''],
+  # ['8', '28th OCT', 'niceMobileHeader'],
+  # ['7', '27th OCT', 'niceHeader'],
   # ['6', '26th OCT', 'adventure'],
   # ['5', '25th OCT', 'carRasing2'],
-  # ['4', '24th OCT', 'carRasing'],
+  ['4', '24th OCT', 'carRasing'],
   ['3', '23rd OCT', 'treasureBoxes2'],
   ['2', '22nd OCT', 'treasureBoxes'],
   ['1', '21st OCT', 'clickNumber'],
