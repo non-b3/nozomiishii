65c42598ee7ca162a93ec15b9dd624216253033a
Nozomi Ishii <non.ankt@gmail.com>
2019-11-02 01:04:53 +0900

    deploy day 13

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ puts 'Deleted all the code data'
   # ['016', '5th NOV', ''],
   # ['015', '4th NOV', ''],
   # ['014', '3rd NOV', 'waniAttack'],
-  # ['013', '2nd NOV', 'carRasing3'],
+  ['013', '2nd NOV', 'carRasing3'],
   ['012', '1st NOV', 'Harapeko'],
   ['011', '31th OCT', 'infected'],
   ['010', '30th OCT', 'forest'],
