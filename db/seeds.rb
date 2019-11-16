c9ffb0416670131b38e321020d30b990b4158355
Nozomi Ishii <non.ankt@gmail.com>
2019-11-17 03:14:27 +0900

    day 29

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ puts 'Deleted all the code data'
 [
   # ['031', '20th NOV', ''],
   # ['030', '19th NOV', ''],
-  # ['029', '18th NOV', ''],
-  # ['028', '17th NOV', 'wordPower'],
+  # ['029', '18th NOV', 'garlic'],
+  ['028', '17th NOV', 'wordPower'],
   ['027', '16th NOV', 'goWatchMovie2'],
   ['026', '15th NOV', 'goWatchMovie'],
   ['025', '14th NOV', 'Dive'],
