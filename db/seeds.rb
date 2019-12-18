6febe9a92fed1e525f1159ec407206fe0fba312d
Nozomi Ishii <non.ankt@gmail.com>
2019-12-18 11:09:00 +0900

    day59;

--- db/seeds.rb
+++ db/seeds.rb
@@ -61,7 +61,7 @@ puts 'Deleted all the code data'
   ['056', '15th DEC', 'nobanasdhi2'],
   ['057', '16th DEC', 'colorBeam'],
   ['058', '17th DEC', 'colorBeam2'],
-  # ['059', '18th DEC', 'colorBeam3'],
+  ['059', '18th DEC', 'colorBeam3'],
   # ['060', '19th DEC', 'koe'],
   # ['061', '20th DEC', 'zombie2'],
   # ['062', '21th DEC', ''],
