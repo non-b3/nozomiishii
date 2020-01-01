6c02febb80e925c18c1c229d43cb42d6663cb428
Nozomi Ishii <non.ankt@gmail.com>
2020-01-02 03:27:23 +0900

    day74

--- db/seeds.rb
+++ db/seeds.rb
@@ -76,7 +76,7 @@ puts 'Deleted all the code data'
   ['071', '30th DEC', '10hours'],
   ['072', '31st DEC', 'monster'],
   ['073', '1st JAN', 'newYear'],
-  # ['074', '2nd JAN', ''],
+  ['074', '2nd JAN', 'desires'],
   # ['075', '3rd JAN', ''],
   # ['076', '4th JAN', ''],
   # ['077', '5th JAN', ''],
