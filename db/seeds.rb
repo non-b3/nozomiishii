ad3169dfee24367f00621fa6f1b05a0e10549073
Nozomi Ishii <non.ankt@gmail.com>
2020-01-18 02:21:54 +0900

    day090

--- db/seeds.rb
+++ db/seeds.rb
@@ -92,7 +92,7 @@ puts 'Deleted all the code data'
   ['087', '15th JAN', 'photo2'],
   ['088', '16th JAN', 'video'],
   ['089', '17th JAN', 'camera'],
-  # ['090', '18th JAN', ''],
+  ['090', '18th JAN', 'capture'],
   # ['091', '19th JAN', ''],
   # ['092', '20th JAN', ''],
   # ['093', '21st JAN', ''],
